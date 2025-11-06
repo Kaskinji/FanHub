51fc2eabde4ec2b90e295bc2c8e260c1a8b6a46b
Accoie <sypherjensqr@gmail.com>
2025-11-06 19:29:32 +0300

    добавил в биндинги

--- server/FanHub/Application/ApplicationBindings.cs
+++ server/FanHub/Application/ApplicationBindings.cs
@@ -12,14 +12,23 @@ namespace Application
         public static IServiceCollection AddApplication(
             this IServiceCollection services )
         {
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IFandomService, FandomService>();
             services.AddScoped<IGameService, GameService>();
+            services.AddScoped<IPostService, PostService>();
+            services.AddScoped<IUserService, UserService>();
 
+            services.AddValidatorsFromAssemblyContaining<CategoryValidator>();
+            services.AddValidatorsFromAssemblyContaining<FandomValidator>();
             services.AddValidatorsFromAssemblyContaining<GameValidator>();
-
             services.AddValidatorsFromAssemblyContaining<PostValidator>();
+            services.AddValidatorsFromAssemblyContaining<UserValidator>();
 
+            services.AddAutoMapper( typeof( CategoryProfile ).Assembly );
+            services.AddAutoMapper( typeof( FandomProfile ).Assembly );
             services.AddAutoMapper( typeof( GameProfile ).Assembly );
             services.AddAutoMapper( typeof( PostProfile ).Assembly );
+            services.AddAutoMapper( typeof( UserProfile ).Assembly );
 
             return services;
         }
